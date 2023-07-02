0bf5ab237f4da439b44b51ff90699ae8521ee967
Lewis <louis.icatlo@gmail.com>
2023-07-02 18:36:01 +0800

    First Commit

---
+++ src/test/java/net/javaguides/sms/StudentManagementSystem01ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystem01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
